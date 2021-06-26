eb9b31d679fca1a5e027950bcf1da4ea2b8990f3
fauzan2720 <fauzan.abdillah2705@gmail.com>
2021-06-26 13:23:27 +0700

    Create build.xml

---
+++ RumahLaundry/src/rumahlaundry/RumahLaundry.java
@@ -0,0 +1,13 @@
+package rumahlaundry;
+
+/**
+ *
+ * @author Fauzan
+ */
+public class RumahLaundry {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}

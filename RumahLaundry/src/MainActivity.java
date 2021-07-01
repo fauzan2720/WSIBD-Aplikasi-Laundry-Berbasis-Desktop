c8dd4ba9aaf781877b6c5b4071a4890ae4b55c7d
fauzan2720 <fauzan.abdillah2705@gmail.com>
2021-07-01 11:56:37 +0700

    Update private.properties

---
+++ RumahLaundry/src/MainActivity.java
@@ -0,0 +1,14 @@
+
+
+/**
+ *
+ * @author Fauzan
+ */
+public class MainActivity {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new halm_dashboard().show();
+    }
+    
+}

47327e65ecf9a55e8ea756fbe2d6fc77c9f90d43
HilalWahyuPriAditya <hilalwahyupriaditya@gmail.com>
2021-07-21 16:42:39 +0700

    Update koneksi.class

--- RumahLaundry/src/koneksi/koneksi.java
+++ RumahLaundry/src/koneksi/koneksi.java
@@ -13,10 +13,7 @@ import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- *
- * @author Eldi
- */
+
 public class koneksi {
     
     public static Connection getConnection() {
